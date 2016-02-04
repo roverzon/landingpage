52dc228bb37d98cb1397ea1aad95694018fd668a
rov01 <roverzon@gmail.com>
2016-02-04 21:50:54 +0800

    switch form account

--- public/js/cta_1.js
+++ public/js/cta_1.js
@@ -1,18 +1,14 @@
 (function() {
   'use strict';
-  window.callback = function(){ console.log("yoyo"); }; 
-  _clForm.loadForm("http://host.51convert.cn", "f45c9e364a0e4a82ab82dbf1049deb1b",{
-      formInit: function(){
-          document.getElementById('clSubmitForm').classList.add('btn');
+    _clForm.loadForm("http://host.convertlab.com", "1120b5771f25406bb120f3ceda42ecb6",{
+    formInit: function(){
+       document.getElementById('clSubmitForm').classList.add('btn');
           document.getElementById('clSubmitForm').classList.add('btn-secondary');
           var d=document.querySelector("input[type=email]");
           d.placeholder = '公司邮箱';
           d.style = 'font-size:20px;';
-      },
-      beforeSubmit: function(){},
-      onSubmit: function(args){
-          console.log(args);
-          window.callback();
-      }
+    },
+    beforeSubmit: function(){},
+    onSubmit: function(args){}
   });
 })();
--- public/js/cta_2.js
+++ public/js/cta_2.js
@@ -1,15 +1,15 @@
 (function(){
   'use strict';
-    _clForm.loadForm("http://host.51convert.cn", "1c0dc3ccd5d54b42a00a5463ea291d52",{
-      formInit: function(){
-        document.getElementById('clSubmitForm').classList.add('btn');
+    _clForm.loadForm("http://host.convertlab.com", "bbb97a4fa3a942709571d7ec3f353188",{
+    formInit: function(){
+       document.getElementById('clSubmitForm').classList.add('btn');
         document.getElementById('clSubmitForm').classList.add('btn-warning');
         document.getElementById('formFieldSet').style.padding = 0;
         var d = document.querySelector("input[type=email]");
         d.placeholder = '公司邮箱';
         d.style = 'font-size:20px;';
-      },
-      beforeSubmit: function(){},
-      onSubmit: function(args){}
+    },
+    beforeSubmit: function(){},
+    onSubmit: function(args){}
   });
 })();
